b3d14f3a37b36993a99199e4a9908274591d561e
leadtrip <woodm@objectcomputing.com>
2022-10-04 12:17:45 +0100

    Testing

--- src/test/groovy/wood/mike/table/per/subclass/CarSpec.groovy
+++ src/test/groovy/wood/mike/table/per/subclass/CarSpec.groovy
@@ -1,18 +1,36 @@
 package wood.mike.table.per.subclass
 
+import grails.gorm.transactions.Transactional
+import grails.test.hibernate.HibernateSpec
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
-class CarSpec extends Specification implements DomainUnitTest<Car> {
-
-    def setup() {
-    }
-
-    def cleanup() {
-    }
+class CarSpec extends HibernateSpec implements DomainUnitTest<Car> {
 
     void "test something"() {
-        expect:"fix me"
-            true == false
+        given:
+            Car car = addCar()
+        when:
+            updateCar(car)
+        then:
+            checkUpdate(car)
+    }
+
+    def checkUpdate(Car car) {
+        Car.withTransaction {
+            Car.get(car.id).numberOfSeats == 4
+        }
+    }
+
+    def updateCar(Car car) {
+        Car.withTransaction {
+            Car.executeUpdate("update Car c set c.numberOfSeats = 4 where c.id in(:ids)", [ids: [car.id]])
+        }
+    }
+
+    def addCar() {
+        Car.withTransaction {
+            new Car(primaryPowerSource: 'engine', numberOfSeats: 5).save()
+        }
     }
 }
